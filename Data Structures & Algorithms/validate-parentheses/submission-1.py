1ba0b69d9cb9b073a3e3746ad2c1094a5a356602
MohammadShahabaaz <32320211+mohammadshahabaaz@users.noreply.github.com>
2026-08-19 02:47:40 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {')':'(', '}':'{', ']':'['}
+
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if top_element != bracket_map[char]:
+                    return False
+                else:
+                    stack.append(char)
+        return len(stack) == 0
