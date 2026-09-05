52eae1b1736488612b7e2655d8d2dbbb897f82f6
MohammadShahabaaz <32320211+mohammadshahabaaz@users.noreply.github.com>
2026-09-06 00:32:49 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st = s.replace(" ", "")
+        L, R = 0, len(s) - 1
+        lowerSt = toLowerCase(st)
+        while L < R:
+            if st[R] != st[L]:
+                return False
+            L += 1
+            R -= 1
+        return True
